94f28d3f0bf8bea1ecd752c30497a91777ec4904
Lorys3008 <lorys3006@gmail.com>
2026-05-20 11:54:48 +0200

    hero dashboard 2

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -4,17 +4,18 @@ export default function Dashboard() {
     return (
         <main>
             <Hero
-                    title="Découvrez les"
-                    subtitle=" "
-                    labelTitle="produits"
-                    spanTitle="disponilbles"
-                    endTitle="Récoltéo"
+                    title=""
+                    subtitle="Découvrez"
+                    labelTitle="très vite"
+                    spanTitle="nos produits"
+                    endTitle="sur Récoltéo"
                     description="Parcourez les produits mis à disposition par nos partenaires et réservez ceux dont votre association a besoin."
                     primaryButton="Voir les produits"
                     primaryButtonHref="#products"
                     secondaryButton="Ajouter un élément"
                     secondaryButtonHref="#dons"
-                  />
+                    />
+            
         </main>
     )
 }
